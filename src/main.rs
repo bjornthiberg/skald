7d44f4cd4ccb98f345b6bdcc60bd65af481bf8dc
Björn Thiberg <bthiberg@kth.se>
2024-11-12 22:02:13 +0100

    feat: add cli commands (#4)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("hello")
+mod cli;
+mod journal;
+
+use clap::Parser;
+use cli::Cli;
+use journal::Storage;
+use std::io::Result;
+use std::path::PathBuf;
+
+fn main() -> Result<()> {
+    let mut storage: Storage = Storage::new(PathBuf::from("journal_data"))?;
+
+    let cli: Cli = Cli::parse();
+
+    match cli.command {
+        Some(command) => command.execute(&mut storage)?,
+        None => println!("No command provided. Use --help for usage information."),
+    }
+
+    Ok(())
 }
